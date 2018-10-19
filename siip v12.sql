5c79b8cfaa896baf9333e398096bca2fb1f9aa19
githhubofedward <37826661+githhubofedward@users.noreply.github.com>
2018-10-20 02:20:18 +0700

    siip v12

--- siip v12.sql
+++ siip v12.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2018 at 08:33 AM
+-- Generation Time: Aug 14, 2018 at 02:47 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -35,10 +35,170 @@ CREATE TABLE IF NOT EXISTS `author` (
   `author_address` text NOT NULL,
   `author_contact` varchar(20) NOT NULL,
   `author_email` varchar(256) NOT NULL,
-  `author_saving_bank` varchar(256) NOT NULL,
-  `author_saving_num` varchar(20) NOT NULL,
+  `bank_id` varchar(4) DEFAULT NULL,
+  `author_saving_num` varchar(30) NOT NULL,
   `heir_name` varchar(256) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`author_id`, `work_unit_id`, `institute_id`, `author_name`, `author_degree`, `author_address`, `author_contact`, `author_email`, `bank_id`, `author_saving_num`, `heir_name`) VALUES
+(4, 1, 1, 'Joko', 'S.T', 'Yogyakarta', '087951245581', 'asd@asd.com', '014', '087542154887', 'Jokowi'),
+(5, 1, 2, 'Susi', 'S.Ked', 'Sleman', '087951245575', 'asd@kakaka.com', '459', '08754215488', 'Susi junior');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE IF NOT EXISTS `bank` (
+  `bank_name` varchar(256) NOT NULL,
+  `bank_id` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bank`
+--
+
+INSERT INTO `bank` (`bank_name`, `bank_id`) VALUES
+('BANK BRI', '002'),
+('BANK EKSPOR INDONESIA', '003'),
+('BANK MANDIRI', '008'),
+('BANK BNI', '009'),
+('BANK DANAMON', '011'),
+('PERMATA BANK', '013'),
+('BANK BCA', '014'),
+('BANK BII', '016'),
+('BANK PANIN', '019'),
+('BANK ARTA NIAGA KENCANA', '020'),
+('BANK NIAGA', '022'),
+('BANK BUANA IND', '023'),
+('BANK LIPPO', '026'),
+('BANK NISP', '028'),
+('AMERICAN EXPRESS BANK LTD', '030'),
+('CITIBANK N.A.', '031'),
+('JP. MORGAN CHASE BANK, N.A.', '032'),
+('BANK OF AMERICA, N.A', '033'),
+('ING INDONESIA BANK', '034'),
+('BANK MULTICOR TBK.', '036'),
+('BANK ARTHA GRAHA', '037'),
+('BANK CREDIT AGRICOLE INDOSUEZ', '039'),
+('THE BANGKOK BANK COMP. LTD', '040'),
+('THE HONGKONG & SHANGHAI B.C.', '041'),
+('THE BANK OF TOKYO MITSUBISHI UFJ LTD', '042'),
+('BANK SUMITOMO MITSUI INDONESIA', '045'),
+('BANK DBS INDONESIA', '046'),
+('BANK RESONA PERDANIA', '047'),
+('BANK MIZUHO INDONESIA', '048'),
+('STANDARD CHARTERED BANK', '050'),
+('BANK ABN AMRO', '052'),
+('BANK KEPPEL TATLEE BUANA', '053'),
+('BANK CAPITAL INDONESIA, TBK.', '054'),
+('BANK BNP PARIBAS INDONESIA', '057'),
+('BANK UOB INDONESIA', '058'),
+('KOREA EXCHANGE BANK DANAMON', '059'),
+('RABOBANK INTERNASIONAL INDONESIA', '060'),
+('ANZ PANIN BANK', '061'),
+('DEUTSCHE BANK AG.', '067'),
+('BANK WOORI INDONESIA', '068'),
+('BANK OF CHINA LIMITED', '069'),
+('BANK BUMI ARTA', '076'),
+('BANK EKONOMI', '087'),
+('BANK ANTARDAERAH', '088'),
+('BANK HAGA', '089'),
+('BANK IFI', '093'),
+('BANK CENTURY, TBK.', '095'),
+('BANK MAYAPADA', '097'),
+('BANK JABAR', '110'),
+('BANK DKI', '111'),
+('BPD DIY', '112'),
+('BANK JATENG', '113'),
+('BANK JATIM', '114'),
+('BPD JAMBI', '115'),
+('BPD ACEH', '116'),
+('BANK SUMUT', '117'),
+('BANK NAGARI', '118'),
+('BANK RIAU', '119'),
+('BANK SUMSEL', '120'),
+('BANK LAMPUNG', '121'),
+('BPD KALSEL', '122'),
+('BPD KALIMANTAN BARAT', '123'),
+('BPD KALTIM', '124'),
+('BPD KALTENG', '125'),
+('BPD SULSEL', '126'),
+('BANK SULUT', '127'),
+('BPD NTB', '128'),
+('BPD BALI', '129'),
+('BANK NTT', '130'),
+('BANK MALUKU', '131'),
+('BPD PAPUA', '132'),
+('BANK BENGKULU', '133'),
+('BPD SULAWESI TENGAH', '134'),
+('BANK SULTRA', '135'),
+('BANK NUSANTARA PARAHYANGAN', '145'),
+('BANK SWADESI', '146'),
+('BANK MUAMALAT', '147'),
+('BANK MESTIKA', '151'),
+('BANK METRO EXPRESS', '152'),
+('BANK SHINTA INDONESIA', '153'),
+('BANK MASPION', '157'),
+('BANK HAGAKITA', '159'),
+('BANK GANESHA', '161'),
+('BANK WINDU KENTJANA', '162'),
+('HALIM INDONESIA BANK', '164'),
+('BANK HARMONI INTERNATIONAL', '166'),
+('BANK KESAWAN', '167'),
+('BANK TABUNGAN NEGARA (PERSERO)', '200'),
+('BANK HIMPUNAN SAUDARA 1906, TBK .', '212'),
+('BANK TABUNGAN PENSIUNAN NASIONAL', '213'),
+('BANK SWAGUNA', '405'),
+('BANK JASA ARTA', '422'),
+('BANK MEGA', '426'),
+('BANK JASA JAKARTA', '427'),
+('BANK BUKOPIN', '441'),
+('BANK SYARIAH MANDIRI', '451'),
+('BANK BISNIS INTERNASIONAL', '459'),
+('BANK SRI PARTHA', '466'),
+('BANK JASA JAKARTA', '472'),
+('BANK BINTANG MANUNGGAL', '484'),
+('BANK BUMIPUTERA', '485'),
+('BANK YUDHA BHAKTI', '490'),
+('BANK MITRANIAGA', '491'),
+('BANK AGRO NIAGA', '494'),
+('BANK INDOMONEX', '498'),
+('BANK ROYAL INDONESIA', '501'),
+('BANK ALFINDO', '503'),
+('BANK SYARIAH MEGA', '506'),
+('BANK INA PERDANA', '513'),
+('BANK HARFA', '517'),
+('PRIMA MASTER BANK', '520'),
+('BANK PERSYARIKATAN INDONESIA', '521'),
+('BANK DIPO INTERNATIONAL', '523'),
+('BANK AKITA', '525'),
+('LIMAN INTERNATIONAL BANK', '526'),
+('ANGLOMAS INTERNASIONAL BANK', '531'),
+('BANK KESEJAHTERAAN EKONOMI', '535'),
+('BANK UIB', '536'),
+('BANK ARTOS IND', '542'),
+('BANK PURBA DANARTA', '547'),
+('BANK MULTI ARTA SENTOSA', '548'),
+('BANK MAYORA', '553'),
+('BANK INDEX SELINDO', '555'),
+('BANK EKSEKUTIF', '558'),
+('CENTRATAMA NASIONAL BANK', '559'),
+('BANK FAMA INTERNASIONAL', '562'),
+('BANK SINAR HARAPAN BALI', '564'),
+('BANK VICTORIA INTERNATIONAL', '566'),
+('BANK HARDA', '567'),
+('BANK FINCONESIA', '945'),
+('BANK MERINCORP', '946'),
+('BANK MAYBANK INDOCORP', '947'),
+('BANK OCBC – INDONESIA', '948'),
+('BANK CHINA TRUST INDONESIA', '949'),
+('BANK COMMONWEALTH', '950');
 
 -- --------------------------------------------------------
 
@@ -122,23 +282,32 @@ CREATE TABLE IF NOT EXISTS `draft` (
   `theme_id` mediumint(9) DEFAULT NULL,
   `draft_title` varchar(256) NOT NULL,
   `draft_file` varchar(256) NOT NULL,
-  `proposed_fund` int(11) NOT NULL,
-  `approved_fund` int(11) NOT NULL,
-  `entry_date` datetime NOT NULL,
-  `finish_date` datetime NOT NULL,
-  `print_date` datetime NOT NULL,
-  `worksheet_serial_num` int(20) NOT NULL,
-  `approved_worksheet` int(20) NOT NULL,
-  `is_reviewed` tinyint(1) NOT NULL,
+  `proposed_fund` int(13) NOT NULL,
+  `approved_fund` int(13) NOT NULL,
+  `entry_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `finish_date` timestamp NULL DEFAULT NULL,
+  `print_date` timestamp NULL DEFAULT NULL,
+  `worksheet_serial_num` varchar(50) NOT NULL,
+  `approved_worksheet` enum('y','n') NOT NULL DEFAULT 'n',
+  `is_reviewed` enum('y','n') NOT NULL DEFAULT 'n',
   `review_notes` text NOT NULL,
-  `is_revised` tinyint(1) NOT NULL,
+  `is_revised` enum('y','n') NOT NULL DEFAULT 'n',
   `revise_notes` text NOT NULL,
-  `is_edited` tinyint(1) NOT NULL,
+  `is_edited` enum('y','n') NOT NULL DEFAULT 'n',
   `edit_notes` text NOT NULL,
-  `is_layouted` tinyint(1) NOT NULL,
-  `is_reprint` tinyint(1) NOT NULL,
-  `draft_notes` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_layouted` enum('y','n') NOT NULL DEFAULT 'n',
+  `layout_notes` text NOT NULL,
+  `is_reprint` enum('y','n') NOT NULL DEFAULT 'n',
+  `draft_notes` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `draft`
+--
+
+INSERT INTO `draft` (`draft_id`, `category_id`, `theme_id`, `draft_title`, `draft_file`, `proposed_fund`, `approved_fund`, `entry_date`, `finish_date`, `print_date`, `worksheet_serial_num`, `approved_worksheet`, `is_reviewed`, `review_notes`, `is_revised`, `revise_notes`, `is_edited`, `edit_notes`, `is_layouted`, `layout_notes`, `is_reprint`, `draft_notes`) VALUES
+(1, 8, 4, 'qweqwe', 'qweqewqeqweqweqwewqe', 10000, 100000000, '2018-08-14 09:10:20', '2018-08-30 09:10:20', '2018-08-14 09:10:20', '212456201212', 'n', 'n', 'sssssss', 'n', 'ddd', 'n', 'ffffff', 'n', 'werwer', 'n', 'fgdgdfg'),
+(3, 2, 5, 'mnmnmnm', 'mnmnmnmnmn', 10121, 45457, '2018-08-14 09:13:17', NULL, NULL, '654656', 'n', 'n', '', 'n', '', 'n', '', 'n', '', 'n', '');
 
 -- --------------------------------------------------------
 
@@ -149,7 +318,7 @@ CREATE TABLE IF NOT EXISTS `draft` (
 CREATE TABLE IF NOT EXISTS `faculty` (
   `faculty_id` mediumint(9) NOT NULL,
   `faculty_name` varchar(256) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculty`
@@ -157,7 +326,7 @@ CREATE TABLE IF NOT EXISTS `faculty` (
 
 INSERT INTO `faculty` (`faculty_id`, `faculty_name`) VALUES
 (1, 'Kehutanan'),
-(3, 'Vokasi');
+(3, 'Teknik');
 
 -- --------------------------------------------------------
 
@@ -190,7 +359,16 @@ CREATE TABLE IF NOT EXISTS `process` (
   `draft_id` mediumint(9) DEFAULT NULL,
   `reviewer_id` mediumint(9) DEFAULT NULL,
   `author_id` mediumint(9) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `process`
+--
+
+INSERT INTO `process` (`process_id`, `draft_id`, `reviewer_id`, `author_id`) VALUES
+(1, 1, 4, 4),
+(2, 1, 4, 5),
+(3, 3, 4, 5);
 
 -- --------------------------------------------------------
 
@@ -214,7 +392,7 @@ CREATE TABLE IF NOT EXISTS `reviewer` (
   `reviewer_id` mediumint(9) NOT NULL,
   `reviewer_name` varchar(256) NOT NULL,
   `faculty_id` mediumint(9) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reviewer`
@@ -228,7 +406,9 @@ INSERT INTO `reviewer` (`reviewer_id`, `reviewer_name`, `faculty_id`) VALUES
 (9, 'Mirna', 3),
 (11, 'Bima', 1),
 (12, 'Jono', 3),
-(13, 'Sandi', 1);
+(13, 'Sandi', 1),
+(14, 'Joko', 3),
+(15, 'Suparjo', 1);
 
 -- --------------------------------------------------------
 
@@ -302,7 +482,7 @@ CREATE TABLE IF NOT EXISTS `worksheet` (
 CREATE TABLE IF NOT EXISTS `work_unit` (
   `work_unit_id` mediumint(9) NOT NULL,
   `work_unit_name` varchar(256) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `work_unit`
@@ -310,8 +490,8 @@ CREATE TABLE IF NOT EXISTS `work_unit` (
 
 INSERT INTO `work_unit` (`work_unit_id`, `work_unit_name`) VALUES
 (1, 'UGM'),
-(2, 'UIN'),
-(4, 'UNY');
+(2, 'UI'),
+(5, 'UNS');
 
 --
 -- Indexes for dumped tables
@@ -323,7 +503,14 @@ INSERT INTO `work_unit` (`work_unit_id`, `work_unit_name`) VALUES
 ALTER TABLE `author`
   ADD PRIMARY KEY (`author_id`),
   ADD KEY `work_unit_id` (`work_unit_id`),
-  ADD KEY `institute_id` (`institute_id`);
+  ADD KEY `institute_id` (`institute_id`),
+  ADD KEY `bank_id` (`bank_id`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`bank_id`);
 
 --
 -- Indexes for table `book`
@@ -422,7 +609,7 @@ ALTER TABLE `work_unit`
 -- AUTO_INCREMENT for table `author`
 --
 ALTER TABLE `author`
-  MODIFY `author_id` mediumint(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `author_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `book`
 --
@@ -442,12 +629,12 @@ ALTER TABLE `code`
 -- AUTO_INCREMENT for table `draft`
 --
 ALTER TABLE `draft`
-  MODIFY `draft_id` mediumint(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `draft_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `faculty_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `faculty_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `institute`
 --
@@ -457,7 +644,7 @@ ALTER TABLE `institute`
 -- AUTO_INCREMENT for table `process`
 --
 ALTER TABLE `process`
-  MODIFY `process_id` mediumint(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `process_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `responsibility`
 --
@@ -467,7 +654,7 @@ ALTER TABLE `responsibility`
 -- AUTO_INCREMENT for table `reviewer`
 --
 ALTER TABLE `reviewer`
-  MODIFY `reviewer_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `reviewer_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `theme`
 --
@@ -487,7 +674,7 @@ ALTER TABLE `worksheet`
 -- AUTO_INCREMENT for table `work_unit`
 --
 ALTER TABLE `work_unit`
-  MODIFY `work_unit_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `work_unit_id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -497,7 +684,8 @@ ALTER TABLE `work_unit`
 --
 ALTER TABLE `author`
   ADD CONSTRAINT `author_ibfk_1` FOREIGN KEY (`work_unit_id`) REFERENCES `work_unit` (`work_unit_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `author_ibfk_2` FOREIGN KEY (`institute_id`) REFERENCES `institute` (`institute_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `author_ibfk_2` FOREIGN KEY (`institute_id`) REFERENCES `institute` (`institute_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `author_ibfk_3` FOREIGN KEY (`bank_id`) REFERENCES `bank` (`bank_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `book`
